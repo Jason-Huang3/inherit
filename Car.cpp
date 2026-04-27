fd3d9ca185965ed4abdc7cbfa833217e97f1fd7a
Jason-Huang3 <jason_huang3@brown.edu>
2026-04-26 21:13:10 -0400

    Car function definitions

---
+++ Car.cpp
@@ -0,0 +1,25 @@
+// Jason Huang
+// CIS 1202 501
+// April 26, 2026
+#include "Vehicle.h"
+#include "Car.h"
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int Car::getDoors() {
+    return Door;
+}
+
+void Car::setDoors(int d) {
+    Door = d;
+}
+
+Car::Car(string m, int y, int d) : Vehicle(m, y){
+    setDoors(d);
+}
+
+void Car::displayInfo() {
+    Vehicle::displayInfo();
+    cout << "Doors: " << getDoors() << endl;
+}
